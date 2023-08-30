e6c62e5e1f94744338ba94afd78c70bc850ec6e5
marzieh kamali <mayhey1@gmail.com>
2023-08-30 04:23:35 +0000

    completed the breedFetcher exercise

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,22 @@
 const request = require('request');
-const input = process.argv.slice(2);
-const breed = input[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, function(error, response, body) {
-  if (error) {
-    console.log("error", error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("breed not found");
-  } else {
-    const firstElement = data[0];
-    console.log(firstElement.description);
-    console.log(typeof data);
-  }
-});
+// give me the breed, when I get the data, I will call you back
+const fetchBreedDescription = function(breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(url, function(error, response, body) {  //async code. request takes a url and a function
+    if (error) {  // if error, call the callback and say im done- here is the erro
+      callback(error, null); // let callback know
+      return;
+    }
+    const data = JSON.parse(body); // data came back
+    if (data.length === 0) { // going through the length
+      callback("breed not found", null); // im done, I will call you, here is the error message, could not find the breed
+    } else {
+      const firstElement = data[0]; // you have data, you call them, there is no erro, here is the description
+      callback(null,firstElement.description);
+    }
+  });
+};
 
+module.exports = { fetchBreedDescription };
 
